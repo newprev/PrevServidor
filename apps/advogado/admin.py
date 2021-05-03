d3d46a904e1fa6af2a7287e86ed1f9db4eef343e
Israel <israel.usp@gmail.com>
2021-05-02 21:12:12 -0300

    Implementando novos serializers
    - Adicionando os serializers do ConvMon(Conversão monetária);
    - Adicionando os serializers do Tetos(Tetos previdenciários);

--- apps/advogado/admin.py
+++ apps/advogado/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Advogado
 
 class AdminAdvogado(admin.ModelAdmin):
-    list_display = ['id', 'nomeUsuario', 'escritorioId', 'login', 'email', 'ativo']
-    list_display_links = ['id', 'escritorioId', 'login', 'email', 'nomeUsuario']
-    list_per_page = 15
+    list_display = ['usuarioId', 'nomeUsuario', 'escritorioId', 'login', 'email', 'ativo']
+    list_display_links = ['usuarioId', 'escritorioId', 'login', 'email', 'nomeUsuario']
     list_editable = ['ativo']
+    readonly_fields = ['usuarioId', ]
+    list_per_page = 15
 
 admin.site.register(Advogado, AdminAdvogado)
 
