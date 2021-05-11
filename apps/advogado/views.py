c43fbbae227bdacc7531fbd32e230bc7893e3f8b
Israel <israel.usp@gmail.com>
2021-05-10 23:29:18 -0300

    Auto stash before merge of "IsraelDev" and "main"

--- apps/advogado/views.py
+++ apps/advogado/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, generics, filters
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from .models import Advogado
-from .serializers import AdvogadoSerializer
+from .serializers import AdvogadoSerializer, ConfirmaAdvogadoSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -13,6 +15,27 @@ class AdvogadosViewSet(viewsets.ModelViewSet):
     search_fields = ['numeroOAB', 'email']
     filterset_fields = ['ativo']
 
+class AdvogadosConfirmacaoViewSet(generics.RetrieveUpdateAPIView):
+    """Exibindo senha provisória de advogado ainda não confirmado"""
+    def get_queryset(self):
+        queryset = None
+        if len(self.kwargs) != 0:
+            queryset = Advogado.objects.filter(usuarioId=self.kwargs['pk'], confirmado=False)
+        return queryset
+
+    def get_object(self):
+        return get_object_or_404(Advogado, pk=self.kwargs['pk'])
+
+    def patch(self, request, *args, **kwargs):
+        advModel = self.get_object()
+        advogado = ConfirmaAdvogadoSerializer(advModel, data=request.data, partial=True)
+        if advogado.is_valid():
+            advogado.save()
+            return JsonResponse(status=201, data=advogado.data)
+        return JsonResponse(status=400, data="Parâmetros errados")
+
+    serializer_class = ConfirmaAdvogadoSerializer
+    http_method_names = ['get', 'patch']
 
 class ListaAdvogadosByEscritorio(generics.ListAPIView):
     """Exibindo os(as) advogados(as) de determinado escritório"""
