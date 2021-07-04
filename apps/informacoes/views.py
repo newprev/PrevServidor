228cf8dd7ebbad74e542349beab4cc9387ef8b75
Israel <israel.usp@gmail.com>
2021-07-04 12:00:59 -0300

    Incluindo arquivos faltantes

--- apps/informacoes/views.py
+++ apps/informacoes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import IndicadoresSerializer
+from .models import Indicadores
 
-# Create your views here.
+class IndicadoresViewSet(viewsets.ModelViewSet):
+    """Exibe todas as correções monetárias cadastradas"""
+    queryset = Indicadores.objects.all()
+    serializer_class = IndicadoresSerializer
