1425764119b5de129f6e11af3384fd24b14340ce
Israel <israel.usp@gmail.com>
2021-10-17 12:30:03 -0300

    Bug ao apresentar data
    - __str__ estava recebendo uma data ao invés de uma string, o que gerava um erro e sequer salvava o informação quería-se adicionar.

--- apps/ferramentas/models.py
+++ apps/ferramentas/models.py
@@ -13,8 +13,8 @@ class ConvMon(models.Model):
     convMonId = models.AutoField(primary_key=True)
     nomeMoeda = models.CharField(max_length=20, blank=False)
     fator = models.BigIntegerField()
-    dataInicial = models.DateTimeField(blank=False)
-    dataFinal = models.DateTimeField(blank=True, null=True)
+    dataInicial = models.DateField(blank=False)
+    dataFinal = models.DateField(blank=True, null=True)
     conversao = models.CharField(max_length=1, choices=CONVERSAO, default='V', null=False)
     moedaCorrente = models.BooleanField(default=False)
     sinal = models.CharField(max_length=10, blank=False)
@@ -29,13 +29,13 @@ class TetosPrev(models.Model):
     db_table = 'tetosPrev'
 
     tetosPrevId = models.AutoField(primary_key=True)
-    dataValidade = models.DateTimeField(blank=False)
+    dataValidade = models.DateField(blank=False)
     valor = models.DecimalField(max_digits=19, decimal_places=2)
     dataUltAlt = models.DateTimeField(default=datetime.now())
     dataCadastro = models.DateTimeField(default=datetime.now())
 
     def __str__(self):
-        return self.dataValidade
+        return f"{self.dataValidade}"
 
 
 class CarenciasLei91(models.Model):
