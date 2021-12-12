315931d1d567723721715456c6dd5f0890ae96ef
Israel <israel.usp@gmail.com>
2021-12-11 21:47:04 -0300

    NEWPREV-54
    Criação e adição de logs ao longo de todo o projeto

--- apps/advogado/views.py
+++ apps/advogado/views.py
@@ -1,38 +1,60 @@
 from rest_framework import viewsets, generics, filters
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404
+
+from logs.logRest import logPrioridade
 from .models import Advogado
 from .serializers import AdvogadoSerializer, ConfirmaAdvogadoSerializer, AuthClientSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
+from prevEnums import Prioridade, TipoLog
+
 
 class AdvogadosViewSet(viewsets.ModelViewSet):
     """Exibindo todos advogados cadastrados"""
-    queryset = Advogado.objects.all()
-    serializer_class = AdvogadoSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
-    ordering_fields = ['nomeUsuario']
-    search_fields = ['numeroOAB', 'email']
-    filterset_fields = ['ativo']
+    try:
+        logPrioridade("GET::api/advogados", tipoLog=TipoLog.rest)
+        queryset = Advogado.objects.all()
+        serializer_class = AdvogadoSerializer
+        filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+        ordering_fields = ['nomeUsuario']
+        search_fields = ['numeroOAB', 'email']
+        filterset_fields = ['ativo']
+
+    except Exception as err:
+        logPrioridade("GET::api/advogados", tipoLog=TipoLog.rest, priodiade=Prioridade.warnings)
 
 class AdvogadosConfirmacaoViewSet(generics.RetrieveUpdateAPIView):
     """Exibindo senha provisória de advogado ainda não confirmado"""
     def get_queryset(self):
         queryset = None
-        if len(self.kwargs) != 0:
-            queryset = Advogado.objects.filter(advogadoId=self.kwargs['pk'])
-        return queryset
+        try:
+            if len(self.kwargs) != 0:
+                advogadoId = self.kwargs['pk']
+                logPrioridade(f"GET::api/advogados/{advogadoId}/confirmacao/")
+                queryset = Advogado.objects.filter(advogadoId=advogadoId)
+
+            return queryset
+        except Exception as err:
+            logPrioridade(f"err::api/advogados/<advogadoId>/confirmacao/", priodiade=Prioridade.warnings)
+            return HttpResponse(status=510)
 
     def get_object(self):
         return get_object_or_404(Advogado, pk=self.kwargs['pk'])
 
     def patch(self, request, *args, **kwargs):
-        advModel = self.get_object()
-        advogado = ConfirmaAdvogadoSerializer(advModel, data=request.data, partial=True)
-        if advogado.is_valid():
-            advogado.save()
-            return JsonResponse(status=201, data=advogado.data)
-        return JsonResponse(status=400, data="Parâmetros errados")
+        try:
+            logPrioridade(f"PATCH::api/advogados/<advogadoId>/confirmacao/")
+            advModel = self.get_object()
+            advogado = ConfirmaAdvogadoSerializer(advModel, data=request.data, partial=True)
+            if advogado.is_valid():
+                logPrioridade(f"UPDATE::AdvogadosConfirmacaoViewSet - {advModel.advogadoId=}", tipoLog=TipoLog.banco)
+                advogado.save()
+                return JsonResponse(status=201, data=advogado.data)
+            return JsonResponse(status=400, data="Parâmetros errados")
+        except Exception as err:
+            logPrioridade(f"err::api/advogados/<advogadoId>/confirmacao/")
+            return HttpResponse(status=510)
 
     serializer_class = ConfirmaAdvogadoSerializer
     http_method_names = ['get', 'patch']
@@ -47,8 +69,17 @@ class ListaAdvogadosByEscritorio(generics.ListAPIView):
     def get_queryset(self):
         queryset = None
         if len(self.kwargs) != 0:
-            queryset = Advogado.objects.filter(escritorioId_id=self.kwargs['pk'])
-        return queryset
+            try:
+                escritorioId = self.kwargs['pk']
+                queryset = Advogado.objects.filter(escritorioId_id=escritorioId)
+                logPrioridade(f"api/escritorio/{escritorioId}/advogado")
+                return queryset
+
+            except Exception as err:
+                logPrioridade(f"err::api/escritorio/<int:pk>/advogado::{err}", priodiade=Prioridade.warnings)
+                return HttpResponse(status=510)
+
+
     serializer_class = AdvogadoSerializer
 
 class AuthPrevClient(generics.RetrieveAPIView):
@@ -58,14 +89,21 @@ class AuthPrevClient(generics.RetrieveAPIView):
     serializer_class = AuthClientSerializer
 
     def get_object(self):
-        login = self.kwargs['login']
-        if login is not None:
-            if login.isdecimal():
-                return get_object_or_404(Advogado, numeroOAB=self.kwargs['login'])
+        try:
+            login = self.kwargs['login']
+            if login is not None:
+                if login.isdecimal():
+                    logPrioridade(f'OAB::api/advogados/auth/{login}')
+                    return get_object_or_404(Advogado, numeroOAB=self.kwargs['login'])
+                else:
+                    logPrioridade(f'Email::api/advogados/auth/{login}')
+                    return get_object_or_404(Advogado, email=self.kwargs['login'], ativo=True)
             else:
-                return get_object_or_404(Advogado, email=self.kwargs['login'], ativo=True)
-        else:
-            JsonResponse(status=400, data="Parâmetros errados")
+                logPrioridade(f'api/advogados/auth/{login}')
+                JsonResponse(status=400, data="Parâmetros errados")
+        except Exception as err:
+            logPrioridade(f'api/advogados/auth/<login>', priodiade=Prioridade.warnings)
+            return HttpResponse(status=510)
 
     def get_queryset(self):
         queryset = self.get_object()
--- apps/informacoes/views.py
+++ apps/informacoes/views.py
@@ -1,33 +1,57 @@
 from rest_framework import viewsets
+
 from .serializers import IndicadoresSerializer, ExpSobrevidaSerializer, IndicesAtuMonetariaSerializer, SalarioMinimoSerializer, IpcaMensalSerializer
 from .models import Indicadores, ExpectativaSobrevida, IndicesAtualizacaoMonetaria, SalarioMinimo, IpcaMensal
+
+from logs.logRest import logPrioridade
+from prevEnums import Prioridade, TipoLog
 
 
 class IndicadoresViewSet(viewsets.ModelViewSet):
     """Exibe todos os possíveis indicadores do CNIS"""
-    queryset = Indicadores.objects.all()
-    serializer_class = IndicadoresSerializer
+    try:
+        logPrioridade("GET::/indicadores", tipoLog=TipoLog.rest)
+        queryset = Indicadores.objects.all()
+        serializer_class = IndicadoresSerializer
+    except Exception as err:
+        logPrioridade("erro::/indicadores", tipoLog=TipoLog.rest, priodiade=Prioridade.erro)
 
 
 class ExpectativaSobrevidaViewSet(viewsets.ModelViewSet):
     """Exibe todas as informações do IBGE pertinentes ao programa"""
-    queryset = ExpectativaSobrevida.objects.all()
-    serializer_class = ExpSobrevidaSerializer
+    try:
+        logPrioridade("GET::/expSobrevida", tipoLog=TipoLog.rest)
+        queryset = ExpectativaSobrevida.objects.all()
+        serializer_class = ExpSobrevidaSerializer
+    except Exception as err:
+        logPrioridade("erro::/expSobrevida", tipoLog=TipoLog.rest, priodiade=Prioridade.erro)
 
 
 class IndicesAtuMonetariaViewSet(viewsets.ModelViewSet):
     """Exibe todos os índices de atualização monetária para cálculo do benefício"""
-    queryset = IndicesAtualizacaoMonetaria.objects.all()
-    serializer_class = IndicesAtuMonetariaSerializer
+    try:
+        logPrioridade("GET::/indiceAtuMonetaria", tipoLog=TipoLog.rest)
+        queryset = IndicesAtualizacaoMonetaria.objects.all()
+        serializer_class = IndicesAtuMonetariaSerializer
+    except Exception as err:
+        logPrioridade("erro::/indiceAtuMonetaria", tipoLog=TipoLog.rest, priodiade=Prioridade.erro)
 
 
 class SalarioMinimoViewSet(viewsets.ModelViewSet):
     """Exibe todos os salários mínimos (R$) no Brasil desde 1994"""
-    queryset = SalarioMinimo.objects.all()
-    serializer_class = SalarioMinimoSerializer
+    try:
+        logPrioridade("GET::/salarioMinimo", tipoLog=TipoLog.rest)
+        queryset = SalarioMinimo.objects.all()
+        serializer_class = SalarioMinimoSerializer
+    except Exception as err:
+        logPrioridade("erro::/salarioMinimo", tipoLog=TipoLog.rest, priodiade=Prioridade.erro)
 
 
 class IpcaMensalViewSet(viewsets.ModelViewSet):
     """Exibe todos os IPCAs mensais desde Janeiro de 2011"""
-    queryset = IpcaMensal.objects.all()
-    serializer_class = IpcaMensalSerializer
+    try:
+        logPrioridade("GET::/ipcaMensal", tipoLog=TipoLog.rest)
+        queryset = IpcaMensal.objects.all()
+        serializer_class = IpcaMensalSerializer
+    except Exception as err:
+        logPrioridade("erro::/ipcaMensal", tipoLog=TipoLog.rest, priodiade=Prioridade.erro)
