b2826a27b4ab67bf923d82b7c1d7418ca776b335
Israel Alves <israel.usp@gmail.com>
2023-05-02 01:26:46 -0300

    Ajuste no código SQL
    Ajuste no script de criação do usuário do banco de dados

--- instalacao/usuarioSQL.sql
+++ instalacao/usuarioSQL.sql
@@ -1,15 +1,15 @@
 /*Script criado por Israel Alves Lucena Gomes em 30/04/2023*/
 
 
---Criação do usuário
+/* Criação do usuário */
 CREATE USER 'NEWPREV'@'localhost' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 CREATE USER 'NEWPREV'@'%.%.%.%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 CREATE USER 'NEWPREV'@'%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 CREATE USER 'NEWPREV'@'0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 CREATE USER 'NEWPREV'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 
--- Concedendo privilégios
+/* Concedendo privilégios */
 GRANT ALL PRIVILEGES ON *.* TO 'NEWPREV'@'localhost';
 
--- Reiniciando
+/* Reiniciando */
 FLUSH PRIVILEGES;
