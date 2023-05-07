b7dee16c9eb85a93be8fb53508bd8cefe720dcb0
Israel Alves <israel.usp@gmail.com>
2023-05-07 02:52:13 -0300

    Ajuste no script de criação do usuário do banco de dados

--- instalacao/usuarioSQL.sql
+++ instalacao/usuarioSQL.sql
@@ -1,5 +1,6 @@
 /*Script criado por Israel Alves Lucena Gomes em 30/04/2023*/
 
+/*
 USE GIDEON;
 CREATE FUNCTION avaliaUsuario ()
 
@@ -16,23 +17,62 @@ IF (SELECT 1 FROM mysql.user WHERE user = 'NEWPREV'LIMIT 1) THEN
 
 ELSE
 	BEGIN
-		/* Criação do usuário */
 		CREATE USER 'NEWPREV'@'localhost' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 		CREATE USER 'NEWPREV'@'%.%.%.%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 		CREATE USER 'NEWPREV'@'%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 		CREATE USER 'NEWPREV'@'0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 		CREATE USER 'NEWPREV'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
 	
-		/* Concedendo privilégios */
 		GRANT ALL PRIVILEGES ON *.* TO 'NEWPREV'@'localhost';
 		GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'%.%.%.%';
 		GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'%';
 		GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'0';
 		GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'0.0.0.0';
 		
-		/* Reiniciando */
 		FLUSH PRIVILEGES;
 	
 	END;
 
-END IF;
+END IF;*/
+
+DELIMITER //
+
+CREATE FUNCTION avaliaUsuario ()
+	USE GIDEON;
+	
+	BEGIN
+
+		IF (SELECT 1 FROM mysql.user WHERE user = 'NEWPREV'LIMIT 1) THEN 
+				
+			DROP USER 'NEWPREV'@'localhost';
+			DROP USER 'NEWPREV'@'%.%.%.%';
+			DROP USER 'NEWPREV'@'%';
+			DROP USER 'NEWPREV'@'0';
+			DROP USER 'NEWPREV'@'0.0.0.0';
+		
+		ELSE
+			/* Criação do usuário */
+			CREATE USER 'NEWPREV'@'localhost' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+			CREATE USER 'NEWPREV'@'%.%.%.%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+			CREATE USER 'NEWPREV'@'%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+			CREATE USER 'NEWPREV'@'0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+			CREATE USER 'NEWPREV'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+		
+			/* Concedendo privilégios */
+			GRANT ALL PRIVILEGES ON *.* TO 'NEWPREV'@'localhost';
+			GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'%.%.%.%';
+			GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'%';
+			GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'0';
+			GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'0.0.0.0';
+			
+			/* Reiniciando */
+			FLUSH PRIVILEGES;
+		
+		END IF;
+		
+		RETURN;
+	END //
+
+DELIMITER ;
+
+
