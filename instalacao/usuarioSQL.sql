875435c6c431674947a19354cb0db1004e2dfa8d
Israel Alves <israel.usp@gmail.com>
2023-05-07 02:15:08 -0300

    Ajuste SQL
    Ajuste no script de criação do usuário do banco de dados

--- instalacao/usuarioSQL.sql
+++ instalacao/usuarioSQL.sql
@@ -1,19 +1,43 @@
 /*Script criado por Israel Alves Lucena Gomes em 30/04/2023*/
 
 
-/* Criação do usuário */
-CREATE USER 'NEWPREV'@'localhost' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
-CREATE USER 'NEWPREV'@'%.%.%.%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
-CREATE USER 'NEWPREV'@'%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
-CREATE USER 'NEWPREV'@'0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
-CREATE USER 'NEWPREV'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+CREATE FUNCTION avaliaUsuario ()
+RETURNS varcahr(20)
 
-/* Concedendo privilégios */
-GRANT ALL PRIVILEGES ON *.* TO 'NEWPREV'@'localhost';
-GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'%.%.%.%';
-GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'%';
-GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'0.0.0.0';
-GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'0';
-
-/* Reiniciando */
-FLUSH PRIVILEGES;
+BEGIN
+	IF EXISTS (
+				SELECT 
+					USER AS usuario
+				FROM
+					mysql.user
+				WHERE
+					usuario = 'NEWPREV'
+			  ) 
+	THEN
+		
+		DROP USER 'NEWPREV'@'localhost';
+		DROP USER 'NEWPREV'@'%.%.%.%';
+		DROP USER 'NEWPREV'@'0';
+		DROP USER 'NEWPREV'@'0.0.0.0';
+	
+	ELSE
+	
+		/* Criação do usuário */
+		CREATE USER 'NEWPREV'@'localhost' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+		CREATE USER 'NEWPREV'@'%.%.%.%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+		CREATE USER 'NEWPREV'@'%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+		CREATE USER 'NEWPREV'@'0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+		CREATE USER 'NEWPREV'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2021__';
+	
+		/* Concedendo privilégios */
+		GRANT ALL PRIVILEGES ON *.* TO 'NEWPREV'@'localhost';
+		GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'%.%.%.%';
+		GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'%';
+		GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'0.0.0.0';
+		GRANT SELECT, UPDATE, INSERT, DELETE ON *.* TO 'NEWPREV'@'0';
+		
+		/* Reiniciando */
+		FLUSH PRIVILEGES;
+	
+	END IF;
+END
