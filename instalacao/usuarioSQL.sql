29f5b7717864651eff238edbe09a9a7a5a8a5b8c
Israel Alves <israel.usp@gmail.com>
2023-05-07 02:57:32 -0300

    Ajuste no script de criação do usuário do banco de dados

--- instalacao/usuarioSQL.sql
+++ instalacao/usuarioSQL.sql
@@ -43,7 +43,6 @@ CREATE FUNCTION avaliaUsuario ()
 	BEGIN
 
 		IF (SELECT 1 FROM mysql.user WHERE user = 'NEWPREV' LIMIT 1) THEN 
-				
 			DROP USER 'NEWPREV'@'localhost';
 			DROP USER 'NEWPREV'@'%.%.%.%';
 			DROP USER 'NEWPREV'@'%';
